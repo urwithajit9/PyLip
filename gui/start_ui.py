6cd08aec1c188c3c7a3a45e53e32be26a694d326
aravindgeek <aravindlinux01@gmail.com>
2014-03-17 22:41:30 +0400

    added video playing and writing capabality

--- gui/start_ui.py
+++ gui/start_ui.py
@@ -1,7 +1,7 @@
 import sys
 from PySide import QtGui,QtCore
 import configWindow,aboutWindow
-from imgp.ImgP import UpdateFrame
+from imgp.ImgP import UpdateFrame,WriteFrame
 import threading,pickle
 class MainWindow(QtGui.QMainWindow):
 	
@@ -9,6 +9,7 @@ class MainWindow(QtGui.QMainWindow):
 		super(MainWindow,self).__init__()
 		self.initUI()
 		self.isCamera = False
+		self.fname = None
 		self.loadConfiguration()
 
 	def initUI(self):
@@ -18,16 +19,9 @@ class MainWindow(QtGui.QMainWindow):
 		self.config = None 
 		self.statusbar = self.statusBar()
 		self.statusbar.showMessage('Application under intial stage of development')
-		
+
 		self.imgLabel = QtGui.QLabel(self)
-		self.imgLabel.text = "Makes me tired. Meet me at hell"
-		self.imgLabel.resize(self.imgLabel.sizeHint())
-		self.imgLabel.show()
 		self.setCentralWidget(self.imgLabel)
-		#self.hbox = QtGui.QHBoxLayout(self)
-		#self.hbox.addStretch(1)
-		#self.hbox.addWidget(self.imgLabel)
-		#self.setLayout(self.hbox)
 
 		self.menubar = self.menuBar()
 		self.fileMenu = self.menubar.addMenu('&File')
@@ -46,6 +40,7 @@ class MainWindow(QtGui.QMainWindow):
 		self.cameraSourceAction = QtGui.QAction(QtGui.QIcon('webcamera.png'),'&WebCamera',self)
 		self.videoSourceMenu.addAction(self.fileSourceAction)
 		self.videoSourceMenu.addAction(self.cameraSourceAction)
+		self.cameraSourceAction.triggered.connect(self.cameraSourceActionTriggered)
 		
 		self.configureAction = QtGui.QAction(QtGui.QIcon('configure.png'),'&Configure',self)
 		self.configureAction.setStatusTip('Configure the application')
@@ -61,19 +56,28 @@ class MainWindow(QtGui.QMainWindow):
 
 		# Video Menu Actions
 		self.playVideoAction = QtGui.QAction(QtGui.QIcon('playvideo.png'),'&PlayVideo',self)
-		self.playVideoAction.setStatusTip('Play from the selected video source')
+		self.playVideoAction.setStatusTip('Play the video source')
 		self.playVideoAction.triggered.connect(self.playVideo)
+		
+		self.stopVideoAction = QtGui.QAction(QtGui.QIcon('stopvideo.png'),'&StopVideo',self)
+		self.stopVideoAction.setStatusTip('Stop Playing Video')
+		self.stopVideoAction.setEnabled(False)
+		self.stopVideoAction.triggered.connect(self.stopVideo)
 
 		self.startRecordAction = QtGui.QAction(QtGui.QIcon('start.png'),'&Record',self)
 		self.startRecordAction.setStatusTip('records video from selected device')
+		self.startRecordAction.triggered.connect(self.startRecord)
 
 		self.stopRecordAction = QtGui.QAction(QtGui.QIcon('stop.png'),'&Stop',self)
 		self.stopRecordAction.setStatusTip('Stop recording')
+		self.stopRecordAction.setEnabled(False)
+		self.stopRecordAction.triggered.connect(self.stopRecord)
 
 		self.generateAudioAction = QtGui.QAction(QtGui.QIcon('generataudio.png'),'&genearateAudio',self)
 		self.generateAudioAction.setStatusTip('Produce the audio')
 
 		self.videoMenu.addAction(self.playVideoAction)
+		self.videoMenu.addAction(self.stopVideoAction)
 		self.videoMenu.addAction(self.startRecordAction)
 		self.videoMenu.addAction(self.stopRecordAction)
 		self.videoMenu.addAction(self.generateAudioAction)
@@ -116,14 +120,54 @@ class MainWindow(QtGui.QMainWindow):
 
 	def playVideo(self):	
 		self.stop = threading.Event()
-		updateFrame = UpdateFrame(self.stop,self.fname,self.isCamera,self.options['camerano'],self.imgLabel)
-		updateFrame.start()
+		self.stopVideoAction.setEnabled(True)
+		self.playVideoAction.setEnabled(False)
+		self.startRecordAction.setEnabled(False)
+		self.generateAudioAction.setEnabled(False)
+		self.updateFrame = UpdateFrame(self.stop,self.fname,self.isCamera,self.options['camerano'],self)
+		self.updateFrame.start()
+
+	def stopVideo(self):
+		self.stopVideoAction.setEnabled(False)
+		self.playVideoAction.setEnabled(True)
+		self.updateFrame.stop_flag = False
+		self.startRecordAction.setEnabled(True)
+		self.generateAudioAction.setEnabled(True)
+	
+	def startRecord(self):
+		self.stop = threading.Event()
+		self.playVideoAction.setEnabled(False)
+		self.startRecordAction.setEnabled(False)
+		self.generateAudioAction.setEnabled(False)
+		self.stopRecordAction.setEnabled(True)
+		self.fname , ok = QtGui.QInputDialog.getText(self,'File Name','Enter video\'s filename :')
+		self.fname = self.fname + '.mpeg'
+		if ok == False:
+			self.stopRecord()
+		else:
+			self.writeFrame = WriteFrame(self.stop,self.fname,self.isCamera,self.options['camerano'],self)
+			self.writeFrame.start()
+
+	def stopRecord(self):
+		self.playVideoAction.setEnabled(True)
+		self.startRecordAction.setEnabled(True)
+		self.generateAudioAction.setEnabled(True)
+		self.stopVideoAction.setEnabled(False)
+		self.writeFrame.stop_flag = False
+
+
+	def updateOneFrame(self,data):
+		self.pixmap = QtGui.QPixmap.fromImage(data)
+		self.imgLabel.setPixmap(self.pixmap)
+		self.imgLabel.resize(self.imgLabel.sizeHint())
+		self.imgLabel.repaint()
 
 	def loadConfiguration(self):
 		fin = open('config.pickle','r')
 		self.options = pickle.load(fin)
 		print self.options
 		fin.close()
+
 def main():
 	app = QtGui.QApplication(sys.argv)
 	mainWindow = MainWindow()
--- imgp/ImgP.py
+++ imgp/ImgP.py
@@ -1,30 +1,104 @@
 import cv2
-import threading
-from PySide import QtGui
-class UpdateFrame(threading.Thread):
-	def __init__(self,stop_flag,fileName,isCamera,cameraNo,lbl):
+import threading,time
+from PySide import QtGui,QtCore
+class UpdateFrame(QtCore.QThread,QtCore.QObject):
+	frameSignal = QtCore.Signal(QtGui.QImage)
+	def __init__(self,stop_flag,fileName,isCamera,cameraNo,wui):
 		super(UpdateFrame,self).__init__()
+		self.wui = wui
+		self.qimg = None
 		self.stop_flag = stop_flag
 		self.fileName = fileName
 		self.isCamera = isCamera
 		self.cameraNo = cameraNo
-		self.label = lbl
-
+		self.frameSignal.connect(self.wui.updateOneFrame)
+ 
 	def run(self):
 		if self.isCamera == True:
 			cap = cv2.VideoCapture(self.cameraNo)
 		else:
 			cap = cv2.VideoCapture(self.fileName)
+		#cap.set(cv2.cv.CV_CAP_PROP_FPS,25)
+		while self.stop_flag:
+			if self.isCamera == False:
+				time.sleep(0.04)
+			ret, frame = cap.read()
+			if frame == None:
+				print 'Video Source reached end or disconnected'
+				self.wui.stopVideo()
+				break
+			d = cv2.cvtColor(frame,cv2.cv.CV_BGR2RGB)
+			self.qimg = QtGui.QImage(d.data,d.shape[1],d.shape[0],QtGui.QImage.Format_RGB888)
+			self.frameSignal.emit(self.qimg)
+		cap.release()
+		self.exit()
+		self.exec_()
+
+class WriteFrame(QtCore.QThread,QtCore.QObject):
+	frameSignal = QtCore.Signal(QtGui.QImage)
+	def __init__(self,stop_flag,fileName,isCamera,cameraNo,wui):
+		super(WriteFrame,self).__init__()
+		self.wui = wui
+		self.qimg = None
+		self.stop_flag = stop_flag
+		self.fileName = fileName
+		self.isCamera = isCamera
+		self.cameraNo = cameraNo
+		self.frameSignal.connect(self.wui.updateOneFrame)
+ 
+	def run(self):
+		if self.isCamera == True:
+			cap = cv2.VideoCapture(self.cameraNo)
+			ret, frame = cap.read()
+			height, width, layers = frame.shape
+			vidWriter = cv2.VideoWriter(self.fileName,cv2.cv.CV_FOURCC('P','I','M','1')	,25,(width,height))
+			while self.stop_flag:
+				print 'reading a video frame'
+				ret, frame = cap.read()
+				vidWriter.write(frame)
+				if frame == None:
+					print 'Video Source reached end or disconnected'
+					self.wui.stopRecord()
+					break
+				d = cv2.cvtColor(frame,cv2.cv.CV_BGR2RGB)
+				self.qimg = QtGui.QImage(d.data,d.shape[1],d.shape[0],QtGui.QImage.Format_RGB888)
+				self.frameSignal.emit(self.qimg)
+			cap.release()
+			vidWriter.release()
+		else:
+			print 'video source must be a camera'
+			
+		self.exit()
+		self.exec_()
+
+class ProcessedFrame(QtCore.QThread,QtCore.QObject):
+	frameSignal = QtCore.Signal(QtGui.QImage)
+	def __init__(self,stop_flag,fileName,isCamera,cameraNo,wui):
+		super(ProcessedFrame,self).__init__()
+		self.wui = wui
+		self.qimg = None
+		self.stop_flag = stop_flag
+		self.fileName = fileName
+		self.isCamera = isCamera
+		self.cameraNo = cameraNo
+		self.frameSignal.connect(self.wui.updateOneFrame)
+ 
+	def run(self):
+		if self.isCamera == True:
+			cap = cv2.VideoCapture(self.cameraNo)
+		else:
+			cap = cv2.VideoCapture(self.fileName)
+		#cap.set(cv2.cv.CV_CAP_PROP_FPS,25)
 		while self.stop_flag:
 			ret, frame = cap.read()
 			if frame == None:
 				print 'Video Source reached end or disconnected'
+				self.wui.stopVideo()
 				break
 			d = cv2.cvtColor(frame,cv2.cv.CV_BGR2RGB)
-			qimg = QtGui.QImage(d.data,d.shape[1],d.shape[0],QtGui.QImage.Format_RGB888)
-			qpm = QtGui.QPixmap.fromImage(qimg)
-			self.label.setPixmap(qpm)
-			self.label.resize(self.label.sizeHint())
-			self.label.repaint()
-
+			self.qimg = QtGui.QImage(d.data,d.shape[1],d.shape[0],QtGui.QImage.Format_RGB888)
+			self.frameSignal.emit(self.qimg)
 		cap.release()
+		self.exit()
+		self.exec_()
+
